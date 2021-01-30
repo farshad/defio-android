eea81e22bea579d88a00389990180fe23e9e15d4
Farshad Ahangari <ahangari@pars-tamin.ir>
2021-01-30 09:20:28 +0330

    set up bottom menu

--- app/src/main/java/io/neoattitude/defio/ui/HomeFragment.kt
+++ app/src/main/java/io/neoattitude/defio/ui/HomeFragment.kt
@@ -1,10 +1,10 @@
 package io.neoattitude.defio.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import io.neoattitude.defio.R
 
 class HomeFragment : Fragment() {
@@ -13,7 +13,6 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 }
--- app/src/main/java/io/neoattitude/defio/ui/MainActivity.kt
+++ app/src/main/java/io/neoattitude/defio/ui/MainActivity.kt
@@ -1,5 +1,10 @@
 package io.neoattitude.defio.ui
 
+import android.view.View
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import io.neoattitude.defio.R
 import io.neoattitude.defio.ui.base.BaseActivity
 
@@ -10,5 +15,19 @@ class MainActivity : BaseActivity() {
     }
 
     override fun businessLogic() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavMenu)
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.homeFragment) {
+                bottomNavigationView.visibility = View.VISIBLE
+            } else {
+                bottomNavigationView.visibility = View.GONE
+            }
+        }
     }
 }
